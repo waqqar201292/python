4f4e499624663b155f1e8d32a82ec15ca0da20df
root <root@ip-172-31-44-9.ap-south-1.compute.internal>
2021-06-16 18:12:23 +0000

    this is fist commit

---
+++ hello.py
@@ -0,0 +1,9 @@
+[200~N1 =int(input("please enter th no. 1\n"))
+N2 =int(input("please enter th no. 2\n"))
+N3 =int(input("please enter th no. 3\n"))
+N4 =int(input("please enter th no. 4\n"))
+N5 =int(input("please enter th no. 5\n"))
+N6 =int(input("please enter th no. 6\n"))
+N7 =int(input("please enter th no. 7\n"))
+number ={N1, N2, N3, N4, N5, N6,N7}
+print(number)
